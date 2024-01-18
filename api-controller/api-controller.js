f0fb0884763b1b220149e9a7abba9a4e44b52874
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-01-18 21:25:51 +0700

    first commit

---
+++ api-controller/api-controller.js
@@ -0,0 +1,15 @@
+const { sql } = require('@vercel/postgres');
+
+const ApiController = {
+    getAll: async(req, res) => {
+        try {
+            const result =
+              await sql`SELECT * FROM pets;`;
+            return res.status(200).json({ result });
+        } catch (error) {
+            return res.status(500).json({ error });
+        }
+    },
+}
+
+module.exports = ApiController
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+const ApiController = require('./api-controller/api-controller');
+
+app.listen(PORT, () => console.log(`server berjalan pada http://localhost:${PORT}`));
+
+app.get('/', ApiController.getAll);
+
+app.post('/', (req, res) => {
+    res.send("post");
+});
+
+app.put('/', (req, res) => {
+    res.send("update");
+});
+
+app.delete('/', (req, res) => {
+    res.send("delete");
+});
