76090c21ee52f5b581a9b17af9f0e9387664ffe8
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-01-18 22:23:12 +0700

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ApiController = require('./api-controller/api-controller');
 
 app.listen(PORT, () => console.log(`server berjalan pada http://localhost:${PORT}`));
 
-app.get('/', ApiController.getAll);
+app.get('/pets', ApiController.getAll);
 
 app.post('/', ApiController.create);
 
@@ -14,4 +14,4 @@ app.put('/', (req, res) => {
     res.send("update");
 });
 
-app.delete('/:id', ApiController.remove);
+app.delete('/pets/:id', ApiController.remove);
