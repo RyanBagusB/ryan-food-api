a5406938c1a55e248414e712b5e61593b44e4c39
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-01-18 22:04:50 +0700

    first commit

--- api-controller/api-controller.js
+++ api-controller/api-controller.js
@@ -3,8 +3,9 @@ const { sql } = require('@vercel/postgres');
 const ApiController = {
     getAll: async(req, res) => {
         try {
-            const pets = await sql`SELECT * FROM Pets;`;
-            return response.status(200).json({ pets });
+            const result =
+              await sql`SELECT * FROM pets;`;
+            return res.status(200).json({ result });
         } catch (error) {
             return res.status(500).json({ error });
         }
