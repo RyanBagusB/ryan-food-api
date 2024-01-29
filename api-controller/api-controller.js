7bf502b4a1f23c979657e88fcdbf6d82d3811545
RyanBagusB <134161510+RyanBagusB@users.noreply.github.com>
2024-01-29 17:42:40 +0700

    Update api-controller.js

--- api-controller/api-controller.js
+++ api-controller/api-controller.js
@@ -28,7 +28,7 @@ const ApiController = {
     remove: async (req, res) => {
         try {
             //const { id } = req.params;
-            res.status(200).json({ message: req.body});
+            res.status(200).json({ message: req.body.petName});
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server Error' });
