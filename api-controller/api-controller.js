92c59ddc151acf927a14aac40ae88a0bb2ffea03
RyanBagusB <134161510+RyanBagusB@users.noreply.github.com>
2024-01-29 18:47:47 +0700

    Update api-controller.js

--- api-controller/api-controller.js
+++ api-controller/api-controller.js
@@ -26,13 +26,22 @@ const ApiController = {
     },
 
     remove: async (req, res) => {
-        try {
-            //const { id } = req.params;
-            res.status(200).json({ message: req.body});
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
+        // try {
+        //     //const { id } = req.params;
+        //     res.status(200).json({ message: req.body});
+        // } catch (error) {
+        //     console.error(error);
+        //     res.status(500).json({ error: 'Internal Server Error' });
+        // }
+            const { petName, ownerName } = req.body;
+            if (!petName || !ownerName) throw new Error('Pet and owner names required');
+            await sql`INSERT INTO Pets (Name, Owner) VALUES (${petName}, ${ownerName});`;
+          } catch (error) {
+            return response.status(500).json({ error: error.message });
+          }
+          
+          const pets = await sql`SELECT * FROM Pets;`
+          return response.status(200).json({ pets });
     },
 
 }
