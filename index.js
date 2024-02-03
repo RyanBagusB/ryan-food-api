98ddfb906a2811e3fbc8fb019dcfb49e36c2c55c
RyanBagusB <134161510+RyanBagusB@users.noreply.github.com>
2024-02-04 01:01:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,19 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const reviewRouter = require('./routes/review.router');
-const menuRouter = require('./routes/menu.router');
 const dishRouter = require('./routes/dish.router');
+
 const homeRouter = require('./routes/home.router');
+const menuRouter = require('./routes/menu.router');
+const drinkRouter = require('./routes/drink.router');
+const reviewRouter = require('./routes/review.router');
 const searchRouter = require('./routes/search.router');
 
-app.use("/api/reviews", reviewRouter);
-app.use("/api/menus", menuRouter);
-app.use("/api/homes", homeRouter);
 app.use("/api/dishes", dishRouter);
+app.use("/api/homes", homeRouter);
+app.use("/api/menus", menuRouter);
+app.use("/api/drinks", drinkRouter);
+app.use("/api/reviews", reviewRouter);
 app.use("/api/search", searchRouter);
 
 app.listen(PORT, () => console.log(`server berjalan pada http://localhost:${PORT}`));
